681343c5e3f619ccffaf529a4d50fc114416988b
galmachar <grzegduel@gmail.com>
2020-09-19 18:05:09 +0200

    finished exercise

---
+++ exercises/display_env_vars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Create a script that, when run, will display the following environment variables to the console:
+#
+#USER
+#HOME
+#TERM
+
+echo -e "Current user is: $USER \nHome dir is: $HOME \nTerminal session is: $TERM"
